9206f08fdd193c0c2037352fad4c7ede757d0003
Radwa Bahaa <163759488+RadwaBahaa@users.noreply.github.com>
2024-06-18 20:05:08 +0300

    Merging Branches

---
+++ DTOs/DTOs/Requests/UpdateStoreRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.DTOs.Requests
+
+{
+    public class UpdateStoreRequestDTO
+    {
+        public string? Status { get; set; }
+
+    }
+}
---
+++ DTOs/DTOs/Suppliers/AddOrUpdateSupplierDTO.cs
@@ -0,0 +1,19 @@
+﻿using DTOs.DTOs.Stores;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.DTOs.Suppliers
+
+{
+    public class AddOrUpdateSupplierDTO
+    {
+        public string? SupplierName { get; set; }
+        public Point? Location { get; set; }
+        public string? Address { get; set; }
+        public AddOrUpdateStoreRequestsDTO SupplierAssets { get; set; }
+    }
+}
