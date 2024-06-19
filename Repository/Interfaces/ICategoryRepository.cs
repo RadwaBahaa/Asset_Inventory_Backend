56237ed71aa58455881e2072e3b7645327f5b010
LujaineAlayoupi <lujainehossam7@gmail.com>
2024-06-19 17:59:59 +0300

    AssetAndCategoryServices

--- Repository/Interfaces/ICategoryRepository.cs
+++ Repository/Interfaces/ICategoryRepository.cs
@@ -4,6 +4,7 @@ namespace Repository.Interfaces
 {
     public interface ICategoryRepository : IGenericRepository<Category>
     {
+        Task<IEnumerable<object>> GetAll();
         public Task<Category> GetOneByID(int id);
         public Task<Category> GetOneByName(string name);
         public Task<List<Category>> SearchByName(string name);
--- Services/Services/Interface/IAssetServices.cs
+++ Services/Services/Interface/IAssetServices.cs
@@ -1,6 +1,22 @@
-﻿namespace Services.Services.Interface
+﻿using DTOs.DTOs.Assets;
+using Models.Models;
+
+
+
+namespace Services.Services.Interface
 {
     public interface IAssetServices
     {
+        public Task<ReadAssetDTO> Create(AddOrUpdateAssetDTO assetDTO);
+
+        public Task<ReadAssetDTO> GetOneByName(string name);
+        public Task<List<Asset>> SearchByName(string name);
+        public Task<List<Asset>> SearchByCategory(Category category);
+
+
+
+        //searchby name and category 
+        public Task<bool> Update(AddOrUpdateAssetDTO assetDTO, int ID);
+        public Task<bool> Delete(int ID);
     }
 }
