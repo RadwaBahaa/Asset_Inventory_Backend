79208249e1a8f637f3bea2ff9ab9dee8ced69948
Radwa Bahaa <163759488+RadwaBahaa@users.noreply.github.com>
2024-07-06 06:46:39 +0300

    Restore file

---
+++ DTOs/DTOs/DeliveryProcesses/StageCompletionStep.cs
@@ -0,0 +1,9 @@
+﻿namespace DTOs.DTOs.DeliveryProcesses
+{
+    public class StageCompletionStep
+    {
+        public decimal Supplying {  get; set; }
+        public decimal Delivering { get; set; }
+        public decimal Inventory { get; set; }
+    }
+}
--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41de3b1a450566cf7a999804c3fade280859874e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd118a144dc1757a64dd32d8d8bb6d2c9ab0e3e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
